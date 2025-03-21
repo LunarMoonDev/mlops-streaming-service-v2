6bc92711c033b8333a45bf6831113e0b6e6ccdcf
Harjit Brar <workandresearchbrar@gmail.com>
2025-03-21 17:19:16 +0800

    feat: add create local model path in model_util

--- deployment-streaming-v2/utils/model_util.py
+++ deployment-streaming-v2/utils/model_util.py
@@ -1,11 +1,23 @@
+import os
 import json
 import base64
 
 import mlflow
 
+def get_model_location(run_id):
+    model_location = os.getenv('MODEL_LOCATION')
+
+    if model_location is not None:
+        return model_location
+    
+    model_bucket = os.getenv('MODEL_BUCKET', 'mlflow-buckets')
+    model_location = f's3://{model_bucket}/{run_id}/artifacts/model'
+
+    return model_location
+
 def load_model(run_id):
-    logged_model = f's3://mlflow-buckets/{run_id}/artifacts/model'
-    model = mlflow.pyfunc.load_model(logged_model)
+    model_path = get_model_location(run_id)
+    model = mlflow.pyfunc.load_model(model_path)
     return model
 
 def base64_decode(encoded_data):
